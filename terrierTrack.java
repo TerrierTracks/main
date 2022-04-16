5f7ef465edd4d22fb3791a00e987d9591431fe72
perronr <102386452+perronr@users.noreply.github.com>
2022-04-16 16:13:01 -0400

    Add files via upload

---
+++ terrierTrack.java
@@ -0,0 +1,33 @@
+
+
+public class terrierTrack {
+	
+	
+	
+	private static void displayLogin(void) {
+		System.out.println("Welcome to Terrier Track!");
+		System.out.println("");
+		System.out.println("To log in, choose option 1.");
+		System.out.println("");
+		System.out.println("Don't have an Account?");
+		System.out.println("To register for one, choose option 2.");
+	}
+	
+	private static void displayHome(void) {
+		System.out.println("Home Page");
+		System.out.println("");
+		System.out.println(" 1.) Buy Stock");
+		System.out.println(" 2.) Sell Stock");
+		System.out.println(" 3.) Search Company");
+		System.out.println(" 4.) Add Company to Favorites");
+		System.out.println(" 5.) Delete Company from Favorites");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayLogin();
+
+	}
+
+}
