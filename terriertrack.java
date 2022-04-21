d143b5b00baf099b07bf83dd5c6b63d65974c18c
perronr <102386452+perronr@users.noreply.github.com>
2022-04-20 22:56:06 -0400

    Update terriertrack.java
    I made a class for Stock with attributes. I made some helper functions for buy and sell. Haven't finished everything

--- terriertrack.java
+++ terriertrack.java
@@ -3,6 +3,40 @@ import java.util.Scanner;
 //main class for our Stock Tracker application
 class terriertrack {
 
+    public class Stock {
+        private String name;
+        private int price;
+        private int shares;
+
+        public Stock(String name, int price) {
+            this.name = name;
+            this.price = price;
+            this.shares = 1;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getPrice() {
+            return this.price;
+        }
+
+        public int getShares() {
+            return this.shares;
+        }
+
+        public void updateShares(int n) {
+            this.shares += n;
+        }
+    }
+
+    static int BALANCE = 1000;
+    static Stock[] favorites = new Stock[5];
+    static Stock[] holdings = new Stock[20];
+
+    static int PRICE = 0;
+
     // displayWelcome - displays the welcome page
     private static void displayWelcome() {
         System.out.println("Wecome to TerrierTrack!");
@@ -25,7 +59,54 @@ class terriertrack {
 
     // Helper methods
 
-    // search - helper method to search for a company
+    // addStock() - helper method to add stock, returns true to indicated success,
+    // false for failure
+
+    private static int findStock(Stock s, Stock[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].equals(s)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean addToList(Stock s, Stock[] list, int n) {
+        int len = list.length;
+        int index = findStock(s, holdings);
+        if (index != -1) {
+            holdings[index].updateShares(n);
+            return true;
+        }
+        if (len < 5) {
+            list[len] = s;
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean buyStock(Stock s, int n) {
+        int p = s.getPrice();
+        if (n * p <= BALANCE && addToList(s, holdings, n)) {
+            BALANCE -= p;
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean sellStock(Stock s, int n) {
+        int p = s.getPrice();
+        int index = findStock(s, holdings);
+        if (index != -1 && n <= s.getShares()) {
+            BALANCE += n * p;
+            holdings[index].updateShares(-n);
+            if (holdings[index].getShares() == 0) {
+                holdings[index] = null;
+            }
+            return true;
+        }
+        return false;
+    }
 
     // Main method
     public static void main(String[] args) {
@@ -66,12 +147,47 @@ class terriertrack {
             } else if (input.equals("2")) {
                 break;
             } else if (input.equals("3")) {
-                break;
+
+                // Get which stock they want to buy
+                String name;
+                Stock s;
+                while (true) {
+                    Scanner value = new Scanner(System.in);
+                    System.out.println("What Stock do you wish to buy?");
+                    name = value.nextLine();
+                    value.close();
+                    s = search(name);
+
+                    if (s == null) {
+                        System.out.println("We cannot find that stock name, please enter another name.");
+                    } else {
+                        break;
+                    }
+                }
+
+                // Input number of shares
+
+                Scanner value2 = new Scanner(System.in);
+                System.out.println("How many shares of " + name + " would you like to buy?");
+                String num = value2.nextLine();
+                value2.close();
+
+                int n = Integer.parseInt(num);
+                boolean success = buyStock(s, n);
+
+                if (success) {
+                    System.out.println("You have successfully bought a share for " + name + ", congratulations.");
+                } else {
+                    System.out.println("Sorry, you don't have enough to buy a stock for " + name + ".");
+                }
             } else if (input.equals("4")) {
-                break;
+
             } else if (input.equals("5")) {
-                break;
+
             } else if (input.equals("6")) {
+
+            } else if (input.equals("7")) {
+                System.out.println("Goodbye!");
                 break;
             } else {
                 System.out.println(input + " is an invalid input. Try again.");
